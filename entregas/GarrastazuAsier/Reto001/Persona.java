7b0b8e18176da076df99dff521c12bf82150c5fd
Asier Garrastazu <asier.gc20072gmail.com>
2026-09-21 11:26:22 +0200

    Creacion clase persona

---
+++ entregas/GarrastazuAsier/Reto001/Persona.java
@@ -0,0 +1,12 @@
+public class Persona {
+
+    private int minutoLlegada;
+
+    public Persona(int minutoLlegada) {
+        this.minutoLlegada = minutoLlegada;
+    }
+
+    public int getMinutoLlegada() {
+        return minutoLlegada;
+    }
+}
