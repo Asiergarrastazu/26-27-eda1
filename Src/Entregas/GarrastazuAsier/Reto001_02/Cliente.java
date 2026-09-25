ec1d8306dd67c4b4edd01adc3adba633384e3de2
Asier Garrastazu <asier.gc20072gmail.com>
2026-09-25 10:23:49 +0200

    Creamos la clase Cliente

--- Src/Entregas/GarrastazuAsier/Reto001_02/Cliente.java
+++ Src/Entregas/GarrastazuAsier/Reto001_02/Cliente.java
@@ -1,5 +1,18 @@
 package Src.Entregas.GarrastazuAsier.Reto001_02;
 
 public class Cliente {
+    private Cliente siguiente;
+
+    public Cliente(){
+        siguiente = null;
+    }
+    
+    public Cliente getSiguiente(){
+        return siguiente;
+    }
+    
+    public void setSiguiente(Cliente siguiente){
+        this.siguiente = siguiente;
+    }
 
 }
